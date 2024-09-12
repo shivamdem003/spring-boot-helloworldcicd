235d8e6ca461a8cb5e56df7e57d7d262152d7ce9
shivamdem003 <1998.saha.shivam@gmail.com>
2024-09-12 20:39:48 +0530

    trying to run the springboot file

--- src/main/java/com/example/gitCICD/CICD_JAVA/controller/HelloController.java
+++ src/main/java/com/example/gitCICD/CICD_JAVA/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.gitCICD.CICD_JAVA.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@CrossOrigin("*")
+@RestController()
 public class HelloController {
 
-    @GetMapping("hello/")
+    @GetMapping()
     public String hello(){
         return "Hello to CICD";
     }
